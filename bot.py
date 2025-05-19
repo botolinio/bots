395c3b2405e5d5610b60c1d1a79bcbac067eeb5d
botolinio <losowy.szymek@gmail.com>
2025-05-19 21:22:15 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,23 +18,35 @@ async def on_ready():
 async def dm(ctx, *, message: str):
     await asyncio.sleep(10)
     try:
-        await ctx.author.send(f'{message}')
         messages_to_send = textwrap.wrap(message, 145)
+        counter = 0
         for chunk in reversed(messages_to_send):
             await asyncio.sleep(5)
             await ctx.author.send(chunk)
+            await asyncio.sleep(5)
+            await ctx.author.send(chunk)
+            if counter == 4:
+                await asyncio.sleep(480)
+            elif counter == 9:
+                await asyncio.sleep(360)
+            counter += 1
     except discord.Forbidden:
         await ctx.send('Nie mogę wysłać wiadomości prywatnej (masz je zablokowane).')
 
 @bot.command()
 async def dm2(ctx, *, message: str):
-    await asyncio.sleep(360)
+    await asyncio.sleep(1320)
     try:
-        await ctx.author.send(f'{message}')
         messages_to_send = textwrap.wrap(message, 145)
+        counter = 1
         for chunk in reversed(messages_to_send):
             await asyncio.sleep(5)
             await ctx.author.send(chunk)
+            await asyncio.sleep(5)
+            await ctx.author.send(chunk)
+            if counter % 5 == 0:
+                await asyncio.sleep(360)
+            counter += 1
     except discord.Forbidden:
         await ctx.send('Nie mogę wysłać wiadomości prywatnej (masz je zablokowane).')
 
